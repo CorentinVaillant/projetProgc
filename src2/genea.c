0574c18c0f09f2a420373849759cb1a1eed7d959
Corentin Vaillant <contact.corentin.vaillant@gmail.com>
2023-11-29 14:48:05 +0100

    commit

--- src2/genea.c
+++ src2/genea.c
@@ -1,26 +1,61 @@
 #include "genea.h"
 #include <stdlib.h>
 
+typedef  struct sFiche * pFiche;
 
 tArbre ArbreCreer(void){
     return malloc(sizeof(struct sArbre));
 }
 
 void ArbreAfficher(tArbre Arbre){
+
+    if(!Arbre->pPremiere || !Arbre->pDerniere){
+        printf("!%d\n", (int) Arbre->pPremiere);
+        printf("!%d\n", (int) Arbre->pDerniere);
+        perror("aaaah");
+        return ;
+    }
+        
     
-    tIdentite fPere = Arbre->pPremiere->pPere;
-    tIdentite fMere = Arbre->pPremiere->pMere;
-    
-    printf("\tPère :");
-    if(!fPere)
-        printf("inconnu");
-    else
-        IdentiteAfficher(fPere->Identifiant);
-    printf("\tMère :");
-    if(!fMere)
-        printf("inconnu");
-    else
-        IdentiteAfficher(fMere->Identifiant);
+    pFiche id = Arbre->pPremiere;
+    pFiche dern = Arbre->pDerniere;
+
+    while (id!=dern){
+        //affichage de l'identité
+        if(!id->Identite) printf("inconnu");
+        else IdentiteAfficher(id->Identite);
+        
+        //affichage du père
+        printf("\n\tPère : ");
+        if(!(id->pPere) || !(id->pPere->Identite)) printf("inconnu");
+        else IdentiteAfficher(id->pPere->Identite);
+
+        //affichage de la mère
+        printf("\n\tMère : ");
+        if(!(id->pMere) || !(id->pMere->Identite)) printf("inconnu");
+        else IdentiteAfficher(id->pMere->Identite);
+        printf("\n");
+
+        if(id == id->pSuivante)
+            return ;
+
+        id = id->pSuivante;
+    }
+
+        //affichage de l'identité
+        if(!dern->Identite) printf("inconnu");
+        else IdentiteAfficher(dern->Identite);
+        
+        //affichage du père
+        printf("\n\tPère : ");
+        if(!(dern->pPere) || !(dern->pPere->Identite)) printf("inconnu");
+        else IdentiteAfficher(dern->pPere->Identite);
+
+        //affichage de la mère
+        printf("\n\tMère : ");
+        if(!(dern->pMere) || !(dern->pMere->Identite)) printf("inconnu");
+        else IdentiteAfficher(dern->pMere->Identite);
+        printf("\n");
     
     
 }
--- src2/testgenea.c
+++ src2/testgenea.c
@@ -1,8 +1,36 @@
 #include "genea.h"
 
+void test(void){
+    tIdentite famille[4] = {
+        IdentiteCreer(1,"pig","pepa",'F',"01/04/2030"),
+        IdentiteCreer(2,"pig","george",'M',"06/01/2032"),
+        IdentiteCreer(3,"pig","papa",'M',"06/06/0006"),
+        IdentiteCreer(4,"hog","mama",'F',"77/77/0077")};
+
+    printf("\x1b[31mfamille :\x1b[0m");
+    for(int i=0 ; i<4 ; i++){
+        printf("\n\t");
+        IdentiteAfficher(famille[i]);
+    }printf("\n");
+
+    struct sFiche papa = {famille[2],NULL};
+    struct sFiche mama = {famille[3],NULL,NULL,&papa};
+    struct sFiche pepa = {famille[0],&papa,&mama,&mama};
+    struct sFiche george = {famille[1],&papa,&mama,&pepa};
+
+    tArbre abrAsaucisse = ArbreCreer();
+    abrAsaucisse->pPremiere = &george;
+    abrAsaucisse->pDerniere = &papa;
+    printf("\x1b[31mArbreAfficher\n\x1b[0m");
+    ArbreAfficher(abrAsaucisse);
+
+    printf("--OK--\n");
+
+}
+
+
 int main(){
     tArbre abr = ArbreCreer();
-
-    printf("\tOK");
+    test();
     return 0;
 }
